1583075d4e652513f7f3ab1d528166ac24cafd10
naopoyo <5816375+naopoyo@users.noreply.github.com>
2023-09-27 22:28:17 +0900

    feat: NabBarに追加したページへのリンクを追加

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
+
 export default function NabBar() {
   return (
     <nav className='h-[64px] sticky top-0 z-10 px-8 bg-black/70 bg-background/70 backdrop-blur-xl flex flex-row justify-center'>
-      <div className='flex flex-auto items-center'>
-        <div className='font-bold text-2xl'>naopoyo</div>
+      <div className='flex flex-auto items-center gap-4'>
+        <div className='font-bold text-2xl'>
+          <Link href='/'>naopoyo</Link>
+        </div>
+        <ul className='flex-auto flex flex-row gap-6 items-center justify-end'>
+          <li className='text-sm'>
+            <Link href='/tags'>Tags</Link>
+          </li>
+          <li className='text-sm'>
+            <Link href='/about'>About</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   )
