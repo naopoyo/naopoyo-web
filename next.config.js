2be76291e6f8a6bde9feed33b68f6dd0bc4a3ff4
naopoyo <5816375+naopoyo@users.noreply.github.com>
2023-10-16 00:08:00 +0900

    feat: Cache-Controlを変更

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, s-maxage=300, stale-while-revalidate=300',
+          },
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
