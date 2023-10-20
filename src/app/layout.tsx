7498ef28e28e96ca516e6e73494d166d2a7afe41
naopoyo <5816375+naopoyo@users.noreply.github.com>
2023-10-21 01:00:11 +0900

    feat: metadataにrobotsを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,10 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'naopoyo',
+  robots: {
+    follow: true,
+    index: true,
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
