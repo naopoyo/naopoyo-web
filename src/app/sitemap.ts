231c1987f988874b4d56067c2558f6d401467f96
naopoyo <5816375+naopoyo@users.noreply.github.com>
2023-10-21 15:31:52 +0900

    feat: sitemapを更新

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,8 +7,8 @@ type Route = {
   lastModified: string
 }
 
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+const baseUrl = process.env.NEXT_PUBLIC_DOMAIN
+  ? `https://${process.env.NEXT_PUBLIC_DOMAIN}`
   : 'http://localhost:3000'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
