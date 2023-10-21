649df4f68340dc2061b2948cafcf402cd5910ac2
naopoyo <5816375+naopoyo@users.noreply.github.com>
2023-10-21 16:35:11 +0900

    feat: metadataBaseを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,12 @@ import NabBar from '@/components/nav-bar/nav-bar'
 
 import type { Metadata } from 'next'
 
+const baseUrl = process.env.NEXT_PUBLIC_DOMAIN
+  ? `https://${process.env.NEXT_PUBLIC_DOMAIN}`
+  : 'http://localhost:3000'
+
 export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
   title: 'naopoyo',
   robots: {
     follow: true,
