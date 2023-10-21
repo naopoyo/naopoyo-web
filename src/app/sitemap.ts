e86eb5a945e0f1d87d8fb8a1b1982228e04527a7
naopoyo <5816375+naopoyo@users.noreply.github.com>
2023-10-21 15:09:58 +0900

    feat: sitemapを追加

---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from 'next'
+
+import { getDocuments } from '@/features/document'
+
+type Route = {
+  url: string
+  lastModified: string
+}
+
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : 'http://localhost:3000'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const routesMap = ['', '/abount', '/tags'].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString(),
+  }))
+
+  const docsPromise = getDocuments().then(({ documents }) =>
+    documents.map((doc) => ({
+      url: `${baseUrl}/docs/${doc.slug}`,
+      lastModified: doc.modifiedAt,
+    }))
+  )
+
+  let fetchedRoutes: Route[] = []
+
+  try {
+    fetchedRoutes = (await Promise.all([docsPromise])).flat()
+  } catch (error) {
+    throw JSON.stringify(error, null, 2)
+  }
+
+  return [...routesMap, ...fetchedRoutes]
+}
