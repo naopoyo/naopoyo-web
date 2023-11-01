c8a6f222cbb3338f085a6da7018018b95447d8ed
naopoyo <5816375+naopoyo@users.noreply.github.com>
2023-11-01 21:33:32 +0900

    feat: ランダム絵文字コピーにogイメージを追加

---
+++ src/app/tools/random-emoji/twitter-image.tsx
@@ -0,0 +1,13 @@
+import { DocumentOpengraphImage } from '@/components/opengraph-image'
+
+export const runtime = 'edge'
+export const alt = 'naopoyo'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+export const contentType = 'image/png'
+
+export default async function TwitterImage() {
+  return DocumentOpengraphImage(size, '🎲', 'ランダム絵文字コピー')
+}
