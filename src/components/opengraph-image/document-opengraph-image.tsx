d2d23fffeeeee5b0508921d8964d2b7662fca4d0
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-01-09 23:15:11 +0900

    feat: DocumentOpengraphImageのデザインを変更

--- src/components/opengraph-image/document-opengraph-image.tsx
+++ src/components/opengraph-image/document-opengraph-image.tsx
@@ -7,22 +7,22 @@ export default async function DocumentOpengraphImage(
 ) {
   return new ImageResponse(
     (
-      <div tw="bg-[#020817] h-full w-full flex items-center justify-center py-8 px-16">
-        <div tw="flex items-center justify-center flex-col w-full h-full gap-6">
-          <div tw="w-24 h-24 text-[92px] leading-none">{emoji}</div>
-          <p tw="flex-auto text-7xl font-black text-center text-[#FFF]">{title}</p>
-          <div tw="flex items-center text-4xl text-center text-[#FFF]">
-            <div
-              style={{
-                background: 'url(https://naopoyo.com/naopoyo2.png)',
-                backgroundSize: '100% 100%',
-                width: '36px',
-                height: '36px',
-                marginRight: 14,
-              }}
-            ></div>
-            <div>naopoyo.com</div>
-          </div>
+      <div tw="bg-[#020817] h-full w-full flex flex-col p-12">
+        <div tw="flex justify-center">
+          <div tw="w-[96px] h-[96px] text-[96px] leading-none">{emoji}</div>
+        </div>
+        <p tw="flex-auto text-7xl font-black flex justify-center items-center text-center text-[#FFF]">
+          {title}
+        </p>
+        <div tw="flex items-center justify-end text-4xl text-[#FFF]">
+          <div
+            style={{
+              background: 'url(https://naopoyo.com/naopoyo2.png)',
+              backgroundSize: '100% 100%',
+            }}
+            tw="w-[36px] h-[36px] mr-2"
+          ></div>
+          <div>naopoyo.com</div>
         </div>
       </div>
     ),
