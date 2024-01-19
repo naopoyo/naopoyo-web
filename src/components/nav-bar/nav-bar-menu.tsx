257aeb27b53f815bcffb106feb6b57f2ecd8e521
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-01-20 01:04:46 +0900

    feat: NavBarを更新

---
+++ src/components/nav-bar/nav-bar-menu.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { Menu } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { cn } from '@/lib/shadcn-utils'
+
+const menuItems = [
+  {
+    label: 'Search',
+    href: '/search',
+  },
+  {
+    label: 'Docs',
+    href: '/',
+  },
+  {
+    label: 'Tags',
+    href: '/tags',
+  },
+  {
+    label: 'Bookmarks',
+    href: '/bookmarks',
+  },
+  {
+    label: 'Tools',
+    href: '/tools',
+  },
+  {
+    label: 'About',
+    href: '/about',
+  },
+]
+
+export default function NavBarMenu() {
+  const pathname = usePathname()
+  const items = menuItems.map((item) => ({
+    ...item,
+    isActive: pathname === item.href,
+  }))
+
+  return (
+    <>
+      <div className="flex flex-auto justify-end md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Menu />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56" align="end">
+            {items.map((item, i) => (
+              <DropdownMenuItem key={`menu-item-${i}`} asChild>
+                <Link className="hover:cursor-pointer" href={item.href}>
+                  {item.label}
+                </Link>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <ul className="hidden flex-auto flex-row items-center justify-end gap-6 md:flex">
+        {items.map((item, i) => (
+          <li
+            key={`menu-item-${i}`}
+            className={cn(
+              'text-sm text-muted-foreground hover:text-primary py-2',
+              item.isActive ? 'border-b' : ''
+            )}
+          >
+            <Link href={item.href}>{item.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -1,42 +1,9 @@
-import { Menu } from 'lucide-react'
 import Link from 'next/link'
 
 import { ThemeToggle } from '@/components/theme-toggle'
-import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { siteName } from '@/constants'
 
-const menuItems = [
-  {
-    label: 'Search',
-    href: '/search',
-  },
-  {
-    label: 'Docs',
-    href: '/',
-  },
-  {
-    label: 'Tags',
-    href: '/tags',
-  },
-  {
-    label: 'Bookmarks',
-    href: '/bookmarks',
-  },
-  {
-    label: 'Tools',
-    href: '/tools',
-  },
-  {
-    label: 'About',
-    href: '/about',
-  },
-]
+import NavBarMenu from './nav-bar-menu'
 
 export default function NavBar() {
   return (
@@ -45,31 +12,8 @@ export default function NavBar() {
         <div className="text-2xl font-bold hover:text-link">
           <Link href="/">{siteName}</Link>
         </div>
-        <div className="flex flex-auto justify-end md:hidden">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
-                <Menu />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end">
-              {menuItems.map((menuItem, i) => (
-                <DropdownMenuItem key={`menu-item-${i}`} asChild>
-                  <Link className="hover:cursor-pointer" href={menuItem.href}>
-                    {menuItem.label}
-                  </Link>
-                </DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-        <ul className="hidden flex-auto flex-row items-center justify-end gap-6 md:flex">
-          {menuItems.map((menuItem, i) => (
-            <li key={`menu-item-${i}`} className="text-sm text-muted-foreground hover:text-primary">
-              <Link href={menuItem.href}>{menuItem.label}</Link>
-            </li>
-          ))}
-        </ul>
+
+        <NavBarMenu />
 
         <ThemeToggle />
       </div>
