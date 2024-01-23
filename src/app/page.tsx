ca82649076b1725556c7aa44982fecd9bc032c53
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-01-23 22:07:02 +0900

    feat: HomePageにSuspenseを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { Suspense } from 'react'
+
 import { DocumentList } from '@/components/document'
 import { PageHeader } from '@/components/page-header'
+import { Skeleton } from '@/components/ui/skeleton'
 import { client } from '@/lib/hackersheet'
 
 const title = 'Docs'
@@ -8,17 +11,35 @@ export const dynamic = 'force-static'
 export const revalidate = 60
 
 export default async function HomePage() {
+  return (
+    <main className="container">
+      <PageHeader>{title}</PageHeader>
+      <Suspense fallback={<DocumentListSectionSkeleton />}>
+        <DocumentListSection />
+      </Suspense>
+    </main>
+  )
+}
+
+async function DocumentListSection() {
   const { documents } = await client.getDocuments({
     filter: { draft: false },
     sort: { by: 'published_at', order: 'desc' },
   })
 
   return (
-    <main className="container">
-      <PageHeader>{title}</PageHeader>
-      <section className="pb-8">
-        <DocumentList documents={documents} />
-      </section>
-    </main>
+    <section>
+      <DocumentList documents={documents} />
+    </section>
+  )
+}
+
+function DocumentListSectionSkeleton() {
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+      {Array.from({ length: 6 }).map((_, index) => (
+        <Skeleton key={`skeleton-item-${index}`} className="h-[300px]" />
+      ))}
+    </div>
   )
 }
