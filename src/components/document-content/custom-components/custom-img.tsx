c0fdb2e5f666aedbab1d7583b1f9e48c83565529
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-06-22 22:22:10 +0900

    feat: 画像を拡大できる機能を追加

--- src/components/document-content/custom-components/custom-img.tsx
+++ src/components/document-content/custom-components/custom-img.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { ExtraProps } from 'react-markdown'
 
+import ImageViewer from './image-viewer'
+
 export default function CustomImg({
   children,
   src,
@@ -9,7 +11,11 @@ export default function CustomImg({
   alt,
 }: JSX.IntrinsicElements['img'] & ExtraProps) {
   if (src && width && height && alt) {
-    return <Image src={src} width={Number(width)} height={Number(height)} alt={alt} />
+    return (
+      <ImageViewer>
+        <Image src={src} width={Number(width)} height={Number(height)} alt={alt} />
+      </ImageViewer>
+    )
   }
 
   return <>{children}</>
