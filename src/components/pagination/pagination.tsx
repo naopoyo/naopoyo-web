f470fbc4ff992b75cdcb32ae46c0e08ab0e752b4
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-06-22 15:33:06 +0900

    feat: Paginationの初期スクロール位置を変更するための更新

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { useEffect, useRef } from 'react'
 
 import {
   Pagination as UIPagination,
@@ -16,6 +17,8 @@ export interface PaginationProps {
 }
 
 export default function Pagination({ totalItems, pageSize }: PaginationProps) {
+  const scrollContainerRef = useRef<HTMLDivElement>(null)
+  const scrollAnchorRef = useRef<HTMLDivElement>(null)
   const totalPages = Math.ceil(totalItems / pageSize)
   const createQueryString = useCreateQueryString({ withPathname: true })
   const searchParams = useSearchParams()
@@ -32,13 +35,21 @@ export default function Pagination({ totalItems, pageSize }: PaginationProps) {
 
   const isActive = (value: number) => value === page
 
+  useEffect(() => {
+    if (!scrollContainerRef.current || !scrollAnchorRef.current) {
+      return
+    }
+    scrollContainerRef.current.scrollLeft = scrollAnchorRef.current.offsetLeft - 16
+  }, [])
+
   return (
-    <div className="flex flex-row items-center">
-      <div className="flex-auto overflow-auto">
+    <div className="flex flex-row items-center gap-2">
+      <div ref={scrollContainerRef} className="flex-auto overflow-auto">
         <UIPagination className="justify-start">
           <PaginationContent>
             {pageItems.map((pageItem) => (
               <PaginationItem key={`paginator-${pageItem.num}`}>
+                {isActive(pageItem.num) && <div ref={scrollAnchorRef} />}
                 <PaginationLink
                   href={isActive(pageItem.num) ? '' : pageItem.href}
                   isActive={isActive(pageItem.num)}
