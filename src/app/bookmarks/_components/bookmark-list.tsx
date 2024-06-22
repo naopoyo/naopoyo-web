142c7ce82e9b73b697697415980daccfbf36f18e
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-06-22 15:31:57 +0900

    feat: BookmarkList更新

--- src/app/bookmarks/_components/bookmark-list.tsx
+++ src/app/bookmarks/_components/bookmark-list.tsx
@@ -23,10 +23,11 @@ export async function BookmarkList({ first, after }: BookmarkListProps) {
         {!isEmpty &&
           websites.map((website) => (
             <li key={website.id}>
-              <BookmarkItem website={website} />
+              <BookmarkListItem website={website} />
             </li>
           ))}
       </ul>
+
       <Pagination totalItems={totalCount} pageSize={first} />
     </>
   )
@@ -38,7 +39,7 @@ export function BookmarkListSkeleton() {
       <Pagination totalItems={1} pageSize={1} />
       {Array.from({ length: 20 }).map((_, index) => (
         <Skeleton key={`bookmark-list-skeleton-${index}`} className="w-full rounded-lg">
-          <div className="h-24 w-full rounded-lg"></div>
+          <div className="h-28 w-full rounded-lg"></div>
         </Skeleton>
       ))}
       <Pagination totalItems={1} pageSize={1} />
@@ -46,44 +47,46 @@ export function BookmarkListSkeleton() {
   )
 }
 
-function BookmarkItem({ website }: { website: WebsiteListeItem }) {
+function BookmarkListItem({ website }: { website: WebsiteListeItem }) {
   const faviconUrl = getFaviconUrl(website.domain)
+  const url = website.url
+  const title = website.title || website.url
+  const description = website.description
+  const domain = website.domain
+  const ogImage = website.ogImage
+  const documents = website.documents
 
   return (
     <div>
       <a
-        href={website.url}
+        href={url}
         className="flex flex-col-reverse overflow-hidden rounded-lg border !no-underline hover:bg-primary/5 md:h-36 md:flex-row"
       >
         <div className="flex flex-auto flex-col overflow-hidden p-4">
-          <div className="line-clamp-2 flex-auto text-primary">{website.title}</div>
-          <div className="mb-2 line-clamp-2 text-xs text-muted-foreground">
-            {website.description}
-          </div>
+          <div className="line-clamp-2 flex-auto text-primary">{title}</div>
+          <div className="mb-2 line-clamp-2 text-xs text-muted-foreground">{description}</div>
           <div className="flex items-center gap-2">
             <picture className="rounded-full dark:bg-foreground">
-              <img src={faviconUrl} alt={`${website.domain} favicon`} width={16} height={16} />
+              <img src={faviconUrl} alt={`${domain} favicon`} width={16} height={16} />
             </picture>
-            <div className="line-clamp-1 text-nowrap text-sm text-muted-foreground">
-              {website.domain}
-            </div>
+            <div className="line-clamp-1 text-nowrap text-sm text-muted-foreground">{domain}</div>
           </div>
         </div>
-        {website.ogImage && website.ogImage.fileUrl && (
+        {ogImage && ogImage.fileUrl && (
           <div className="md:max-w-[50%]">
             <Image
-              alt={website.title}
-              src={website.ogImage.fileUrl}
-              height={website.ogImage.height}
-              width={website.ogImage.width}
+              alt={title}
+              src={ogImage.fileUrl}
+              height={ogImage.height}
+              width={ogImage.width}
               className="aspect-auto max-h-72 max-w-full object-contain md:max-h-36 md:max-w-min"
             />
           </div>
         )}
       </a>
-      {website.documents.length > 0 && (
+      {documents.length > 0 && (
         <div className="px-2 py-1">
-          {website.documents.map(
+          {documents.map(
             (doc) =>
               !doc.draft && (
                 <div key={doc.id} className="line-clamp-1 text-nowrap text-xs">
