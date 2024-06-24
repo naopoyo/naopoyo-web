a979f40935a5b4547edf0a3c71c53694cf2d1ab4
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-06-24 21:21:27 +0900

    feat: ナビゲーションのハンバーガーメニューを廃止

--- src/components/nav-bar/nav-bar-menu.tsx
+++ src/components/nav-bar/nav-bar-menu.tsx
@@ -1,16 +1,8 @@
 'use client'
 
-import { Menu } from 'lucide-react'
 import Link from 'next/link'
 import { useSelectedLayoutSegment } from 'next/navigation'
 
-import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { cn } from '@/lib/shadcn-utils'
 
 const menuItems = [
@@ -45,38 +37,18 @@ export default function NavBarMenu() {
   }))
 
   return (
-    <>
-      <div className="flex flex-auto justify-end md:hidden">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon" aria-label="Menu Button">
-              <Menu />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56" align="end">
-            {items.map((item, i) => (
-              <DropdownMenuItem key={`menu-item-${i}`} asChild>
-                <Link className="hover:cursor-pointer" href={`/${item.segment}`}>
-                  {item.label}
-                </Link>
-              </DropdownMenuItem>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-      <ul className="hidden flex-auto flex-row items-center justify-end gap-6 md:flex">
-        {items.map((item, i) => (
-          <li
-            key={`menu-item-${i}`}
-            className={cn(
-              'text-sm text-muted-foreground hover:text-primary py-2',
-              item.isActive ? 'border-b' : ''
-            )}
-          >
-            <Link href={`/${item.segment}`}>{item.label}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul className="flex items-center gap-6">
+      {items.map((item, i) => (
+        <li
+          key={`menu-item-${i}`}
+          className={cn(
+            'text-sm text-muted-foreground hover:text-primary py-2',
+            item.isActive ? 'border-b' : ''
+          )}
+        >
+          <Link href={`/${item.segment}`}>{item.label}</Link>
+        </li>
+      ))}
+    </ul>
   )
 }
--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -6,14 +6,21 @@ import NavBarMenu from './nav-bar-menu'
 
 export default function NavBar() {
   return (
-    <header className="container sticky top-0 z-10 flex h-navbar flex-row justify-center backdrop-blur-xl">
-      <div className="flex flex-auto items-center gap-4">
-        <div className="text-2xl font-bold hover:text-link">
-          <Link href="/">{SITE_NAME}</Link>
-        </div>
+    <>
+      <header className="container flex h-navbar flex-row justify-center backdrop-blur-xl sm:sticky sm:top-0 sm:z-10">
+        <div className="flex flex-auto items-center gap-4">
+          <div className="text-2xl font-bold hover:text-link">
+            <Link href="/">{SITE_NAME}</Link>
+          </div>
 
+          <div className="hidden flex-auto justify-end sm:flex">
+            <NavBarMenu />
+          </div>
+        </div>
+      </header>
+      <div className="container sticky top-0 z-10 overflow-auto py-2 backdrop-blur-xl sm:hidden">
         <NavBarMenu />
       </div>
-    </header>
+    </>
   )
 }
