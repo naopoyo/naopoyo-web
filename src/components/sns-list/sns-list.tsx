893b9730adc302e9f68a519df5fa8703b163bcc2
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-07-10 01:57:25 +0900

    feat: SnsList更新

--- src/components/sns-list/sns-list.tsx
+++ src/components/sns-list/sns-list.tsx
@@ -7,7 +7,7 @@ export default function SnsList() {
   return (
     <FlexRow className="gap-2">
       <Button className="rounded-full border" variant="ghost" size="icon" asChild>
-        <NextLink href="https://github.com/naopoyo" aria-label="GitHubのLink">
+        <NextLink href="https://github.com/naopoyo" aria-label="GitHubへのLink">
           <svg viewBox="0 0 98 96" className="size-[24px] dark:hidden">
             <use xlinkHref="/github-mark.svg#a"></use>
           </svg>
@@ -17,16 +17,23 @@ export default function SnsList() {
         </NextLink>
       </Button>
       <Button className="rounded-full border" variant="ghost" size="icon" asChild>
-        <NextLink href="https://twitter.com/naopoyo_tw" aria-label="XのLink">
+        <NextLink href="https://twitter.com/naopoyo_tw" aria-label="XへのLink">
           <svg viewBox="0 0 300 300.251" className="size-[18px] fill-foreground">
             <use xlinkHref="/x-logo.svg#a"></use>
           </svg>
         </NextLink>
       </Button>
       <Button className="rounded-full border" variant="ghost" size="icon" asChild>
-        <NextLink href="https://sizu.me/naopoyo" aria-label="しずかなインターネットのLink">
+        <NextLink href="https://sizu.me/naopoyo" aria-label="しずかなインターネットへのLink">
           <svg className="size-[24px] fill-foreground">
             <use xlinkHref="/sizu-me-logo.svg#a"></use>
+          </svg>
+        </NextLink>
+      </Button>
+      <Button className="rounded-full border" variant="ghost" size="icon" asChild>
+        <NextLink href="https://zenn.dev/naopoyo" aria-label="ZennへのLink">
+          <svg className="size-[24px] fill-foreground">
+            <use xlinkHref="/zenn-logo.svg#a"></use>
           </svg>
         </NextLink>
       </Button>
