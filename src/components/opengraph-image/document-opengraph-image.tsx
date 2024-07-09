f5371e27a402117a8e34d529f87bd845d4c272ce
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-07-09 20:53:11 +0900

    feat: Opengraph Imageのデザイン変更

--- src/components/opengraph-image/document-opengraph-image.tsx
+++ src/components/opengraph-image/document-opengraph-image.tsx
@@ -1,34 +1,47 @@
 import { ImageResponse } from 'next/og'
 
+const boldFont = fetch(new URL('/public/NotoSansJP-Bold.otf', import.meta.url)).then((res) =>
+  res.arrayBuffer()
+)
+
 export default async function DocumentOpengraphImage(
   size: { width: number; height: number },
   emoji: string,
   title: string
 ) {
+  const [boldFontData] = await Promise.all([boldFont])
+
   return new ImageResponse(
     (
-      <div tw="bg-[#020817] h-full w-full flex flex-col p-12">
-        <div tw="flex justify-center">
-          <div tw="w-[96px] h-[96px] text-[96px] leading-none">{emoji}</div>
-        </div>
-        <p tw="flex-auto text-7xl font-black flex justify-center items-center text-center text-[#FFF]">
-          {title}
-        </p>
-        <div tw="flex items-center justify-end text-4xl text-[#FFF]">
-          <div
-            style={{
-              background: 'url(https://naopoyo.com/naopoyo2.png)',
-              backgroundSize: '100% 100%',
-            }}
-            tw="w-[36px] h-[36px] mr-2"
-          ></div>
-          <div>naopoyo.com</div>
+      <div tw="bg-[#FBB161] h-full w-full flex flex-col p-10">
+        <div tw="bg-white h-full w-full flex flex-col p-8 rounded-3xl">
+          <div tw="flex-auto text-6xl font-body font-bold flex flex-col justify-start items-start text-left text-black">
+            <div tw="w-[96px] h-[96px] text-[96px] mb-8 leading-none">{emoji}</div>
+            <div>{title}</div>
+          </div>
+          <div tw="flex items-center justify-end text-xl font-bold text-black py-4">
+            <div
+              style={{
+                background: 'url(https://naopoyo.com/naopoyo2.png)',
+                backgroundSize: '100% 100%',
+              }}
+              tw="w-[48px] h-[48px] mr-3"
+            ></div>
+            <div>naopoyo.com</div>
+          </div>
         </div>
       </div>
     ),
     {
       ...size,
       emoji: 'noto',
+      fonts: [
+        {
+          name: 'NotoSansJP',
+          data: boldFontData,
+          weight: 700,
+        },
+      ],
     }
   )
 }
