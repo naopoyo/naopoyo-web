67ec80dca825243c473c3542ea002a1ab7fe72e0
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-07-13 03:18:19 +0900

    feat: コードブロックのクリップボードへのコピー時にshikiのコメントを取り除く

--- src/components/document-content/code-block/code-block-copy-button.tsx
+++ src/components/document-content/code-block/code-block-copy-button.tsx
@@ -10,15 +10,17 @@ export interface CodeBlockCopyButtonProps {
 export default function CodeBlockCopyButton({ code }: CodeBlockCopyButtonProps) {
   const [copied, setCopied] = useState(false)
 
+  const removeShikiCode = (code: string) => code.replace(/ *\/\/.*\[!code[^\]]+\]/gm, '').trim()
+
   const handleClick = () => {
-    navigator.clipboard.writeText(code).then(() => {
+    navigator.clipboard.writeText(removeShikiCode(code)).then(() => {
       setCopied(true)
       setTimeout(() => setCopied(false), 1000)
     })
   }
 
   return (
-    <button className="rounded p-1 hover:bg-white/30" onClick={handleClick}>
+    <button className="rounded p-1 hover:bg-foreground/10" onClick={handleClick}>
       {copied ? <HiCheck size={18} /> : <HiOutlineClipboardDocumentList size={18} />}
     </button>
   )
