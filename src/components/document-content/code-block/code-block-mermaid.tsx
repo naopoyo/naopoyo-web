9b85257a7bdff6a1eac0005b0c03ae217823a367
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-07-15 02:21:30 +0900

    feat: Mermaidのレンダリング処理を変更

--- src/components/document-content/code-block/code-block-mermaid.tsx
+++ src/components/document-content/code-block/code-block-mermaid.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { createHash } from 'crypto'
+
 import mermaid from 'mermaid'
 import { useTheme } from 'next-themes'
 import { useEffect, useRef, useState } from 'react'
@@ -8,18 +10,37 @@ export interface CodeBlockMermaidProps {
   code: string
 }
 
+function createId(code: string) {
+  const hash = createHash('sha256')
+  hash.update(code)
+  return 'id-' + hash.digest('hex')
+}
+
 export default function CodeBlockMermaid({ code }: CodeBlockMermaidProps) {
-  const [mounted, setMounted] = useState(false)
   const ref = useRef<HTMLDivElement>(null)
+  const [mounted, setMounted] = useState(false)
+  const [svg, setSvg] = useState('')
   const { theme, systemTheme } = useTheme()
-  const currentTheme = theme === 'system' ? systemTheme : theme
+  const id = createId(code)
 
   useEffect(() => {
-    setMounted(true)
-    if (mounted && ref.current) {
-      mermaid.init({ theme: currentTheme }, ref.current)
+    const renderMermaid = async () => {
+      if (!mounted) {
+        setMounted(true)
+      }
+      const currentTheme = theme === 'system' ? systemTheme : theme
+      if (mounted && ref.current) {
+        mermaid.initialize({ theme: currentTheme })
+        try {
+          const result = await mermaid.render(id, code, ref.current)
+          setSvg(result.svg)
+        } catch {
+          setSvg('Mermaid Syntax Error')
+        }
+      }
     }
-  }, [mounted, currentTheme, setMounted])
+    renderMermaid()
+  }, [mounted, code, id, theme, systemTheme, setSvg, setMounted])
 
   if (!mounted)
     return (
@@ -29,10 +50,12 @@ export default function CodeBlockMermaid({ code }: CodeBlockMermaidProps) {
     )
 
   return (
-    <div className="my-4 flex items-center justify-center overflow-auto rounded text-[#1D1F21]">
-      <div className="max-h-full max-w-full [&>svg]:w-[600px]" ref={ref}>
-        {code}
-      </div>
+    <div className="my-4 flex items-center justify-center overflow-auto rounded text-foreground">
+      <div
+        className="max-h-full max-w-full [&>svg]:w-[600px]"
+        dangerouslySetInnerHTML={{ __html: svg }}
+        ref={ref}
+      />
     </div>
   )
 }
