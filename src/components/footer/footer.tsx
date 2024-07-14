3da4b683db3aa2bd0ed4da3e0a824f4c65ba8b08
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-07-14 17:45:27 +0900

    feat: Footerのレイアウトを変更

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitcher } from '@/components/theme-switcher'
 
 export default function Footer() {
   return (
-    <footer className="container flex flex-col-reverse items-center justify-center gap-8 pb-4 pt-20 sm:flex-row sm:pt-40">
+    <footer className="container flex flex-col-reverse items-center justify-center gap-8 pb-4 pt-20 sm:flex-row sm:items-end sm:pt-40">
       <div className="flex flex-auto flex-col items-center justify-center gap-8 sm:items-start">
         <SnsList />
         <p className="align-middle">© naopoyo</p>
