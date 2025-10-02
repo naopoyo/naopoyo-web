cc5e31b9061f9f37821785b718017f88bf322fd1
naopoyo <5816375+naopoyo@users.noreply.github.com>
2025-10-02 22:56:45 +0900

    fix: フッターのスタイルを改善
    - フッターのコンテナにmx-autoを追加し、中央揃えを強化
    - フッター内の要素のgapを8から2に変更し、間隔を調整

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,14 +5,14 @@ export default function Footer() {
   return (
     <footer
       className={`
-        container flex flex-col-reverse items-center justify-center gap-8 pt-20
-        pb-4
+        container mx-auto flex flex-col-reverse items-center justify-center
+        gap-8 pt-20 pb-4
         sm:flex-row sm:items-end sm:pt-40
       `}
     >
       <div
         className={`
-          flex flex-auto flex-col items-center justify-center gap-8
+          flex flex-auto flex-col items-center justify-center gap-2
           sm:items-start
         `}
       >
