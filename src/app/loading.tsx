dc4f1a2765787b6b09dc06fe19106e963d1e3f39
naopoyo <5816375+naopoyo@users.noreply.github.com>
2025-10-11 05:16:11 +0900

    feat(loading): ローディングコンポーネントを追加
    - 新しいローディングコンポーネントを作成し、Spinnerを使用して表示

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Spinner } from '@/components/ui/spinner'
+
+export default function RootLoading() {
+  return (
+    <div className="w-full">
+      <Spinner className="mx-auto size-10" />
+    </div>
+  )
+}
