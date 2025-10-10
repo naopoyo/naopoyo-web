ab3deffea377601405d8ae18c5ca4614aaa9be51
naopoyo <5816375+naopoyo@users.noreply.github.com>
2025-10-11 02:10:34 +0900

    feat(full-profile): フルプロフィールコンポーネントを追加
    - AboutPageにFullProfileコンポーネントを追加し、ユーザー情報を表示するように変更しました。
    - 不要なコードを削除し、可読性を向上させました。
    - FullProfileコンポーネントを新規作成し、アバターやSNSリストを表示する機能を実装しました。

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,7 @@ import { Metadata } from 'next'
 
 import { Container } from '@/components/layout'
 import { PageHeader } from '@/components/layout'
-import { Link } from '@/components/link'
-import { Avatar, SnsList } from '@/components/site'
-import { Heading, Paragraph as P } from '@/components/typography'
-import { SITE_DESC } from '@/constants'
+import { FullProfile } from '@/components/site'
 
 const title = 'About'
 const description = 'naopoyo.comについて説明しているページです。'
@@ -15,51 +12,11 @@ export const metadata: Metadata = {
   description: description,
 }
 
-export default function AbountPage() {
+export default function AboutPage() {
   return (
     <Container className="flex flex-col gap-8 pt-16">
       <PageHeader title={title} description={description} />
-
-      <div
-        className={`
-          flex flex-col items-center justify-center gap-8 pb-8
-          md:flex-row md:items-start
-        `}
-      >
-        <div className="w-fit">
-          <Avatar size="lg" />
-        </div>
-        <div className="flex max-w-sm flex-col items-start gap-8">
-          <section>
-            <Heading>naopoyo</Heading>
-            <div className="text-muted-foreground">
-              <P>{SITE_DESC}</P>
-              <P>そのほかにUnityでのゲーム開発も少しずつ始めています。</P>
-              <P>
-                このサイトのソースコードを
-                <Link href="https://github.com/naopoyo/naopoyo-web" icon="external">
-                  GitHub
-                </Link>
-                で公開しているので、Next.jsでブログサイトを作成したい方はぜひ参考にしてみてください。
-              </P>
-            </div>
-          </section>
-
-          <section>
-            <Heading>つくったもの</Heading>
-            <P>
-              <Link href="https://hackersheet.com" icon="external">
-                Hacker Sheet
-              </Link>
-            </P>
-          </section>
-
-          <section className="space-y-4">
-            <Heading>SNS</Heading>
-            <SnsList />
-          </section>
-        </div>
-      </div>
+      <FullProfile />
     </Container>
   )
 }
