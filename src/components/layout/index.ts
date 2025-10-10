106178bf01957adab7e32099c484e2d298965e5f
naopoyo <5816375+naopoyo@users.noreply.github.com>
2025-10-11 00:49:01 +0900

    feat(layout): Footerコンポーネントを新規作成し、インポートを修正
    - Footerコンポーネントを新たに作成し、SNSリストとテーマスイッチャーを追加
    - layout.tsxでFooterのインポート元を修正
    - footer.test.tsxを新規作成し、Footerコンポーネントのテストを追加
    - site/index.tsでSnsListのインポートパスを修正

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,4 @@
 export { default as Container } from './container'
 export { default as Section } from './section'
 export { default as PageHeader } from './page-header'
+export { default as Footer } from './footer'
--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,4 +1,3 @@
-export { default as SnsList } from '@/components/site/sns-list'
+export { default as SnsList } from './sns-list'
 export { default as Avatar } from './avatar'
-export { default as Footer } from './footer'
 export { default as Profile } from './profile'
