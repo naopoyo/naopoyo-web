27afe532e0ecd49de803054549341ec7b4427ae9
naopoyo <5816375+naopoyo@users.noreply.github.com>
2025-10-14 00:33:52 +0900

    style(next.config): Cache-Controlヘッダーの値を修正
    Cache-Controlヘッダーの値を's-maxage=1, stale-while-revalidate=59'から'public, s-maxage=1, stale-while-revalidate=59'に変更しました。

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 's-maxage=1, stale-while-revalidate=59',
+            value: 'public, s-maxage=1, stale-while-revalidate=59',
           },
         ],
       },
