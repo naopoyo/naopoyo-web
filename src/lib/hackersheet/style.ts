d4be40f3306b682b247827b9c8c31d58cf2e3252
naopoyo <5816375+naopoyo@users.noreply.github.com>
2025-10-27 00:48:11 +0900

    style(document-content): h2-h6のリンク前にマージンとコンテンツを追加
    - h2-h6のリンク前にマージンを追加し、コンテンツを設定
    - マークダウンアラートのタイトルにSVGの色を設定

--- src/lib/hackersheet/style.ts
+++ src/lib/hackersheet/style.ts
@@ -1,3 +1,4 @@
-import documentContentStyle from '@hackersheet/react-document-content-styles/basic'
+// import documentContentStyle from '@hackersheet/react-document-content-styles/basic'
+import documentContentStyle from '@/styles/document-content.module.scss'
 
 export { documentContentStyle }
