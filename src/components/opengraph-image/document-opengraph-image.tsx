7303c22f3f60e1068723c834306503d948a51493
naopoyo <5816375+naopoyo@users.noreply.github.com>
2026-01-23 16:24:01 +0900

    refactor(opengraph-image): document-opengraph-imageのマークアップをシンプル化
    - ImageResponse呼び出しの余分な括弧を削除
    - Tailwind classの数値を省略形に統一（[96px] → w-24, [48px] → w-12）

--- src/components/opengraph-image/document-opengraph-image.tsx
+++ src/components/opengraph-image/document-opengraph-image.tsx
@@ -20,26 +20,24 @@ export default async function DocumentOpengraphImage(
   const [boldFontData] = await Promise.all([boldFont])
 
   return new ImageResponse(
-    (
-      <div tw="bg-[#FBB161] h-full w-full flex flex-col p-10">
-        <div tw="bg-white h-full w-full flex flex-col p-8 rounded-3xl">
-          <div tw="flex-auto text-6xl font-body font-bold flex flex-col justify-start items-start text-left text-black">
-            <div tw="w-[96px] h-[96px] text-[96px] mb-8 leading-none">{emoji}</div>
-            <div>{title}</div>
-          </div>
-          <div tw="flex items-center justify-end text-xl font-bold text-black py-4">
-            <div
-              style={{
-                background: 'url(https://naopoyo.com/naopoyo2.png)',
-                backgroundSize: '100% 100%',
-              }}
-              tw="w-[48px] h-[48px] mr-3"
-            ></div>
-            <div>naopoyo.com</div>
-          </div>
+    <div tw="bg-[#FBB161] h-full w-full flex flex-col p-10">
+      <div tw="bg-white h-full w-full flex flex-col p-8 rounded-3xl">
+        <div tw="flex-auto text-6xl font-body font-bold flex flex-col justify-start items-start text-left text-black">
+          <div tw="w-24 h-24 text-[96px] mb-8 leading-none">{emoji}</div>
+          <div>{title}</div>
+        </div>
+        <div tw="flex items-center justify-end text-xl font-bold text-black py-4">
+          <div
+            style={{
+              background: 'url(https://naopoyo.com/naopoyo2.png)',
+              backgroundSize: '100% 100%',
+            }}
+            tw="w-12 h-12 mr-3"
+          ></div>
+          <div>naopoyo.com</div>
         </div>
       </div>
-    ),
+    </div>,
     {
       ...size,
       emoji: 'noto',
