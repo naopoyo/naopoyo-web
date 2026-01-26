2826d2c3af3dfaa5ae2f86ddd1e50fc5580fd497
naopoyo <5816375+naopoyo@users.noreply.github.com>
2026-01-26 10:31:38 +0900

    docs(shadcn-utils): TSDoc を追加

--- src/lib/shadcn-utils.ts
+++ src/lib/shadcn-utils.ts
@@ -1,6 +1,29 @@
+/**
+ * @module shadcn-utils
+ * @description Tailwind CSS クラス名を結合・マージするユーティリティモジュール
+ */
+
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Tailwind CSS と clsx を使用してクラス名を結合し、競合するクラス名を自動的にマージします。
+ *
+ * @param inputs - 結合するクラス値（文字列、オブジェクト、配列、条件式など）
+ * @returns マージされたクラス名文字列
+ *
+ * @example
+ * ```tsx
+ * // 基本的な使用方法
+ * cn('px-2 py-1', 'px-3')  // => 'py-1 px-3'
+ *
+ * // 条件付きクラス
+ * cn('base-class', isActive && 'active-class', disabled && 'disabled')
+ *
+ * // オブジェクト形式
+ * cn({ 'text-red': error, 'text-green': success })
+ * ```
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
