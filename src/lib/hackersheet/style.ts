33d971489e2f390594deab7e019aec33f1342cd1
naopoyo <5816375+naopoyo@users.noreply.github.com>
2026-02-01 13:56:03 +0900

    chore(pnpm): @hackersheet パッケージを更新
    - @hackersheet/core: 0.1.0-alpha.12 → 0.1.0-alpha.13
    - @hackersheet/next-document-content-components: 0.1.0-alpha.30 → 0.1.0-alpha.31
    - @hackersheet/next-document-content-kifu: 0.1.0-alpha.17 → 0.1.0-alpha.18
    - @hackersheet/react-document-content: 0.1.0-alpha.14 → 0.1.0-alpha.15
    - @hackersheet/react-document-content-styles: 0.1.0-alpha.14 → 0.1.0-alpha.15
    - スタイルの import 先を @hackersheet/react-document-content-styles/basic に切り替え

--- src/lib/hackersheet/style.ts
+++ src/lib/hackersheet/style.ts
@@ -1,4 +1,4 @@
-// import documentContentStyle from '@hackersheet/react-document-content-styles/basic'
-import documentContentStyle from '@/styles/document-content.module.scss'
+import documentContentStyle from '@hackersheet/react-document-content-styles/basic'
+// import documentContentStyle from '@/styles/document-content.module.scss'
 
 export { documentContentStyle }
